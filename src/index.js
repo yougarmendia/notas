d6e3e0cf1811067e85b8ff1cd122023d63f9f078
You Garmendia <you.garmendia.ayala@ciisa.cl>
2022-04-28 01:35:10 -0400

    tercera subida

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-require('dotenv').config() // .env llega la URL coded
+require('dotenv').config() // toma las variables de ambientes desde .dotenv
 const app = require('./app')
 
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => {console.log(`This marvelous server is listening you on port ${PORT}`)})
+app.listen(PORT, () => {
+  console.log(`This marvelous server is listening you on port ${PORT}`)
+})
